6fc226119994b282ff2881c9164436a06df54f34
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2025-01-12 01:49:42 +0800

    added multiplication and division feature

--- calculator.py
+++ calculator.py
@@ -8,6 +8,8 @@ operation = input('''
 Enter the number of the operation you would like to perform:
 [1] addition
 [2] subtraction
+[3] multiplication
+[4] division
 ''')
 
 first_num = int(input('Enter your first number: '))
@@ -22,6 +24,16 @@ elif operation == '2':
     # Subtraction
     result_subtraction = first_num - second_num
     print('{} + {} = {}'.format(first_num, second_num, result_subtraction))
+
+elif operation == '3':
+    # Multiplication
+    result_multiplication = first_num * second_num
+    print('{} + {} = {}'.format(first_num, second_num, result_multiplication))
+
+elif operation == '4':
+    # Division
+    result_division = first_num / second_num
+    print('{} + {} = {}'.format(first_num, second_num, result_division))
 
 else:
     print('Option does not exist')
